a6a6433f128c2543200974d830ae93d74cd17d80
luhongyu <812855328@qq.com>
2018-01-03 12:49:12 +0800

    2018-01-03

---
+++ Generate_Dataset/Log Analysis/utils.py
@@ -0,0 +1,57 @@
+# coding=utf-8
+import json
+import pandas as pd
+import re
+
+# loadlogs
+# get_news_quality
+# get_news_topic
+# get_newsid
+# utils.DataSet
+
+def loadlogs():
+    logfiles = json.load(open("logfiles.json"))
+    anslist = []
+    for tf in logfiles:
+        anslist.append(json.load(open(tf)))
+    return anslist
+
+df_news_info = pd.read_excel("exp_list_1221.xlsx")
+newsinfo = {}
+for ti, trow in df_news_info.iterrows():
+    newsinfo[str(trow['newsid'])] = {
+        "quality": trow['control_quality'],
+        "title": trow['title'],
+        "topic": trow['topic']
+    }
+
+def get_news_quality(newsid):
+    return int(newsinfo[str(newsid)]['quality'])
+
+def get_news_topic(newsid):
+    return newsinfo[str(newsid)]['topic']
+
+def get_newsid(turl):
+    tl = re.findall("news/(\d+)", turl)
+    if tl:
+        return tl[0]
+
+
+class DataSet:
+    """
+    .add(data, "colname")
+    .to_pandas
+    """
+    def __init__(self):
+        self.column_datas = {}
+        self.column_names = []
+
+    def add(self, data, colname):
+        if colname not in self.column_datas:
+            self.column_datas[colname] = []
+            self.column_names.append(colname)
+
+        self.column_datas[colname].append(data)
+
+    def to_pandas(self):
+        return pd.DataFrame(self.column_datas)[self.column_names]
